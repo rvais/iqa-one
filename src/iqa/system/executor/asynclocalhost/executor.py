8c28d779f7487fb88cfbe3d419729eb39f0a13f4
Roman Vais <rvais@redhat.com>
2020-11-18 13:52:59 +0100

    Fix typing annotations and imports in asynclocalhost/executor.py

--- src/iqa/system/executor/asynclocalhost/executor.py
+++ src/iqa/system/executor/asynclocalhost/executor.py
@@ -1,17 +1,23 @@
-from typing import Optional
 import asyncio
 
-from iqa.system.executor.localhost.execution import ExecutionAsyncio
-from iqa.system.executor.executor import ExecutorBase
-from iqa.system.command import CommandBase
+from iqa.system.executor.asynclocalhost.execution import ExecutionAsyncio
+from iqa.system.executor.base.executor import ExecutorBase
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import Optional
+    from iqa.system.command.command_base import CommandBase
 
 
 class ExecutorAsyncio(ExecutorBase):
     """ Executor implementation for localhost AsyncIO executions
     """
-    def __init__(self, user: str = 'root', password: str = None, **kwargs) -> None:
+    implementation: str = "async"
+    name: str = 'Executor class for asynchronous localhost execution'
 
-        super().__init__(**kwargs)
+    def __init__(self, user: str = 'root', password: Optional[str] = None, **kwargs) -> None:
+
+        super(ExecutorAsyncio).__init__(**kwargs)
         self._user = user
         self._password = password
 
