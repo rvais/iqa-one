399668f4aa392fb6c780a2d4b0ff3d17be8ed4c1
Roman Vais <rvais@redhat.com>
2020-11-23 12:25:40 +0100

    Rebuild service factory

--- src/iqa/system/service/service_factory.py
+++ src/iqa/system/service/service_factory.py
@@ -1,18 +1,16 @@
 import logging
-from typing import Optional, TYPE_CHECKING
+from typing import TYPE_CHECKING
 
 from iqa.system.command.command_base import CommandBase
-from iqa.system.executor.docker.executor_docker import ExecutorDocker
-from iqa.system.executor.ansible.executor_ansible import ExecutorAnsible
-from iqa.system.executor.execution import ExecutionBase
-from iqa.system.service.service import Service
-from .service_artemis import ServiceFakeArtemis
-from .service_docker import ServiceDocker
-from .service_system_init import ServiceSystemInit
-from .service_systemd import ServiceSystemD
+from iqa.system.service.base.service import Service
+from iqa.system.service.system_d.service_systemd import ServiceSystemD
+from iqa.system.service.system_init.service_system_init import ServiceSystemInit
+from iqa.utils.walk_package import walk_package_and_import
 
 if TYPE_CHECKING:
-    from iqa.utils.types import ExecutorType
+    from typing import Optional, List, Type
+    from iqa.system.executor.base.executor import ExecutorBase
+    from iqa.system.executor.base.execution import ExecutionBase
 
 
 class ServiceFactory(object):
@@ -28,53 +26,72 @@ class ServiceFactory(object):
     """
 
     _logger: logging.Logger = logging.getLogger(__name__)
+    __known_implementations: List[Type[Service]] = walk_package_and_import(__path__, Service)
 
     @staticmethod
     def create_service(
-        executor: 'ExecutorType', service_name: Optional[str] = None, **kwargs
+            executor: ExecutorBase,
+            service_name: Optional[str] = None,
+            **kwargs
     ) -> Service:
+        srv: Optional[Service] = None
+        cls: Optional[Type] = None
         if service_name:
-            # Validate if systemd is available
-            svc_cmd_exec: ExecutionBase = executor.execute(
-                CommandBase(['pidof', 'systemd'], stdout=True, timeout=30)
-            )
-            if svc_cmd_exec.completed_successfully():
-                # Create ServiceSystemD
-                ServiceFactory._logger.debug(
-                    'Creating ServiceSystemD - name: %s - executor: %s'
-                    % (service_name, executor.__class__.__name__)
+            # Try to find specific implementation by name
+            for cls in ServiceFactory.__known_implementations:
+                if cls.__name__ == service_name:
+                    srv = cls(executor=executor, name=service_name, **kwargs)
+                    break
+
+            # If service instance haven't been created yet, try to get one with systemD or SystemInit
+            if srv is None:
+                # Check if systemD is available
+                svc_cmd_exec: ExecutionBase = await executor.execute(
+                    CommandBase(['pidof', 'systemd'], stdout=True, timeout=30)
                 )
-                return ServiceSystemD(name=service_name, executor=executor)
-            else:
-                ServiceFactory._logger.debug(
-                    'Creating ServiceSystemInit - name: %s - executor: %s'
-                    % (service_name, executor.__class__.__name__)
-                )
-                return ServiceSystemInit(name=service_name, executor=executor)
+                if svc_cmd_exec.completed_successfully():
+                    # SystemD is available so create service using that
+                    ServiceFactory._logger.debug(
+                        'Creating ServiceSystemD - name: %s - executor: %s'
+                        % (service_name, executor.__class__.__name__)
+                    )
+                    srv = ServiceSystemD(name=service_name, executor=executor, **kwargs)
+                else:
+                    # SystemD is not available so try to create service using alternative (SystemInit)
+                    ServiceFactory._logger.debug(
+                        'Creating ServiceSystemInit - name: %s - executor: %s'
+                        % (service_name, executor.__class__.__name__)
+                    )
+                    srv = ServiceSystemInit(name=service_name, executor=executor)
         else:
-            container_name: Optional[str] = None
-            if isinstance(executor, ExecutorDocker):
-                container_name = executor.container_name
-            elif (
-                isinstance(executor, ExecutorAnsible)
-                and executor.ansible_connection == 'docker'
-            ):
-                container_name = executor.ansible_host
-            elif isinstance(executor, ExecutorAnsible):
-                ServiceFactory._logger.debug(
-                    'Creating ServiceArtemis - name: %s - executor: %s'
-                    % (service_name, executor.__class__.__name__)
-                )
-                return ServiceFakeArtemis(
-                    name=service_name, executor=executor, **kwargs
-                )
+            # Name of wanted service has not been provided, use executor type to create service.
+            for cls in ServiceFactory.__known_implementations:
+                impl: str = 'implementation'
+                docker: str = 'docker'
+                container: str = 'container_name'
+                ans_conn: str = 'ansible_connection'
+                ans_host: str = 'ansible_host'
 
-            if container_name:
-                ServiceFactory._logger.debug(
-                    'Creating ServiceDocker - name: %s - executor: %s'
-                    % (container_name, executor.__class__.__name__)
-                )
-                return ServiceDocker(name=container_name, executor=executor)  # type: ignore
+                if getattr(cls, impl, '') == docker and getattr(executor, impl, '') == docker:
+                    service_name = getattr(executor, container, service_name)
+                    break
 
-        ServiceFactory._logger.debug('Unable to determine Service')
-        raise ValueError('Unable to determine service for server component')
+                elif getattr(cls, impl, '') == docker and getattr(executor, ans_conn, '') == docker:
+                    service_name = getattr(executor, ans_host, service_name)
+                    break
+
+                elif getattr(cls, impl, 'ServiceImplementation') == getattr(executor, impl, 'ExecutorImplementation'):
+                    break
+                else:
+                    cls = None
+
+            if cls is None:
+                raise ValueError('Unable to determine the kind of service based on given arguments.')
+
+            ServiceFactory._logger.debug(
+                'Creating service %s with name: %s and executor: %s'
+                % (cls.__name__, service_name, executor.__class__.__name__)
+            )
+            srv = cls(executor=executor, name=service_name, **kwargs)
+
+        return srv
