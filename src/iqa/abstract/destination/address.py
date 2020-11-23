64b0770e7e0c1d3871002a8cf261339f8479ba65
Roman Vais <rvais@redhat.com>
2020-11-23 15:31:45 +0100

    Update Address, Queue and RoutingType classes

--- src/iqa/abstract/destination/address.py
+++ src/iqa/abstract/destination/address.py
@@ -1,4 +1,9 @@
-from iqa.abstract.destination.routing_type import RoutingType
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import Optional, List, Union
+    from iqa.abstract.destination.routing_type import RoutingType
+    from iqa.abstract.destination.queue import Queue
 
 
 class Address:
@@ -6,11 +11,34 @@ class Address:
     Address class
     """
 
-    def __init__(self, name: str, routing_type: RoutingType) -> None:
+    def __init__(
+        self,
+        name: str,
+        routing_type: RoutingType,
+        queues: Optional[Union[Queue, List[Queue]]] = None
+    ) -> None:
         self.name: str = name
         self.routing_type: RoutingType = routing_type
-        self._queues: list = list()
+        self._queues: List[Queue]
+        if isinstance(queues, list):
+            self._queues = queues.copy()
+        elif queues is None:
+            self._queues = list()
+        else:
+            self._queues = [queues]
 
     @property
     def queues(self) -> list:
-        return self._queues
+        return self._queues.copy()
+
+    def add_queue(self, queue: Queue) -> None:
+        self._queues.append(queue)
+
+    def has_queue(self, queue: Union[Queue, str]) -> bool:
+        if isinstance(queue, str):
+            for q in self._queues:
+                if q.name == queue:
+                    return True
+            return False
+        return queue in self._queues
+
--- src/iqa/abstract/destination/queue.py
+++ src/iqa/abstract/destination/queue.py
@@ -1,17 +1,36 @@
 """
 Represents a generic Queue entity.
 """
-from iqa.abstract.destination.address import Address
-from iqa.abstract.destination.routing_type import RoutingType
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import Optional, List, Union
+    from iqa.abstract.destination.routing_type import RoutingType
+    from iqa.abstract.destination.address import Address
 
 
 class Queue:
-    def __init__(self, name: str, routing_type: RoutingType, address: Address) -> None:
+    def __init__(self, name: str, routing_type: RoutingType, address: Optional[Address] = None) -> None:
         self.name: str = name
         self.routing_type: RoutingType = routing_type
-        self.address: Address = address
+        self._address: Address = address
         self.message_count: int = 0
 
     @property
     def fqqn(self) -> str:
+        if self.address is None:
+            raise ValueError("This queue haven't been assigned any address.")
+
         return '%s::%s' % (self.address.name, self.name)
+
+    @property
+    def address(self) -> Optional[Address]:
+        return self._address
+
+    @address.setter
+    def set_address(self, address: Address):
+        if self._address is not None:
+            raise AttributeError("Queue %s already has assigned address %s. Address cannot be reassigned.")
+        if address is None:
+            raise ValueError("Expected Address object, None given.")
+        self._address = address
