f307b399529c0370808b9200de474e2805eb3807
Roman Vais <rvais@redhat.com>
2021-01-26 15:39:49 +0100

    Fix node factory

--- src/iqa/system/node/node_factory.py
+++ src/iqa/system/node/node_factory.py
@@ -2,50 +2,58 @@ import logging
 
 from iqa.utils.walk_package import walk_package_and_import
 from iqa.utils.utils import get_subclass_with_prop_value
-# from iqa.system.executor.executor_factory import ExecutorFactory
+from iqa.system.executor.executor_factory import ExecutorFactory
 from iqa.system.node.base.node import Node
+from iqa.system.node import __package__
 
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from typing import Optional, List, Type
+    from iqa.system.executor.base.executor import ExecutorBase
 
 
 class NodeFactory(object):
     logger = logging.getLogger(__name__)
-    __known_implementations: List[Type[Node]] = walk_package_and_import(__path__, Node)
+    __known_implementations: 'List[Type[Node]]' = walk_package_and_import(__package__, Node)
 
     @staticmethod
-    def get_known_implementations() -> List[Type[Node]]:
+    def get_known_implementations() -> 'List[Type[Node]]':
         return NodeFactory.__known_implementations.copy()
 
     @staticmethod
     def create_node(
         hostname: str = 'localhost',
-        executor_type: str = 'local',
-        ip: Optional[str] = None,
+        ip: 'Optional[str]' = None,
+        executor: 'Optional[ExecutorBase]' = None,
         **kwargs
     ) -> Node:
         """
         Creates a Node object based on provided arguments.
         :param hostname:
-        :param executor_type:
+        :param executor:
         :param ip:
         :param kwargs:
         :return:
         """
+
+        if executor is None:
+            executor = ExecutorFactory.create_executor(implementation='local')
+
+        executor_type: str = executor.implementation()
+
         try:
             node = get_subclass_with_prop_value(
                 superclass=Node,
-                cls_property_val='implementation',
-                in_class_property=executor_type
+                in_class_property='implementation',
+                cls_property_val=executor_type
             )
 
             NodeFactory.logger.info(
                 'Creating %s [hostname=%s, host=%s]'
                 % (node.__class__.__name__, hostname, ip)
             )
-            return node(hostname=hostname, ip=ip, **kwargs)
+            return node(hostname=hostname, executor=executor, ip=ip, **kwargs)
         except ValueError:
             NodeFactory.logger.error(
                 'Implementation of specific node with "%s" executor was not found!'
