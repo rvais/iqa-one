107e4a94b9fa381b00ef4dc22ec205539310137c
Roman Vais <rvais@redhat.com>
2021-01-26 15:36:27 +0100

    Fix 'walk_package_and_import' function
    Fixed module loading mechanism. F-strings used for string formatting.

--- src/iqa/utils/walk_package.py
+++ src/iqa/utils/walk_package.py
@@ -9,10 +9,10 @@ if TYPE_CHECKING:
 
 
 def walk_package_and_import(
-        package_path: Union[PathLike[Any], str],
-        superclass: Optional[Type] = None,
-        skip_modules: Optional[List[PathLike[Any]]] = None
-) -> List[Type]:
+        package_path: 'Union[PathLike[Any], str]',
+        superclass: 'Optional[Type]' = None,
+        skip_modules: 'Optional[List[PathLike[Any]]]' = None
+) -> 'List[Type]':
     """Recursively walk the supplied package to retrieve all plugins
        :param package_path: path to package that is supposed to be iterated
        over to find and import submodules. Usually it is __path__ variable
@@ -27,9 +27,15 @@ def walk_package_and_import(
     if skip_modules is None:
         skip_modules = []
 
-    files: Iterator[str] = resources.contents(package_path)
+    if isinstance(package_path, list):
+        for pp in package_path:
+            results.extend(walk_package_and_import(pp, superclass, skip_modules))
+        return results
+
+    files: List[str] = list(resources.contents(package_path))
+
     modules: List[str] = [f[:-3] for f in files if f.endswith(".py") and f[0] != "_"]
-    submodules: List[str] = [f[:-3] for f in files if not f.endswith(".py")]
+    submodules: List[str] = [f for f in files if not f.endswith(".py")]
 
     # Filter out submodules to skip
     for skip in skip_modules:
@@ -37,10 +43,12 @@ def walk_package_and_import(
 
     # import modules from subpackages/submodules
     for subm in submodules:
-        results.extend(walk_package_and_import(subm, superclass, skip_modules))
+        module_path: str = f"{package_path}.{subm}"
+        results.extend(walk_package_and_import(module_path, superclass, skip_modules))
 
     for mod in modules:
-        imported = importlib.import_module(mod)
+        module_path: str = f"{package_path}.{mod}"
+        imported = importlib.import_module(module_path)
         classes = inspect.getmembers(imported, inspect.isclass)
         if superclass is not None:
             results.extend([cls for _, cls in classes if isinstance(cls, superclass)])
