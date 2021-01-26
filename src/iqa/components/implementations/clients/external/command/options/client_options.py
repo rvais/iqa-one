f066ff047463f9e9185fc5cf9f8cf4ecf6e9d96f
Roman Vais <rvais@redhat.com>
2021-01-26 14:25:19 +0100

    Surround typing annotations with single quotes external/command/options/client_options.py
    Typing annotatios of objects that are not imported for the runtime but
    serve only for static type checking must be surrounded in singlequotes
    if used in method signatures.

--- src/iqa/components/implementations/clients/external/command/options/client_options.py
+++ src/iqa/components/implementations/clients/external/command/options/client_options.py
@@ -21,7 +21,7 @@ class ClientOptionsBase(object):
     in an external client command.
     """
 
-    def valid_options(self) -> List[Union[Toggle, Prefixed, KWOption, ListOption]]:
+    def valid_options(self) -> 'List[Union[Toggle, Prefixed, KWOption, ListOption]]':
         """
         Delegated method that must return a list of all valid
         options allowed by the client command.
@@ -64,17 +64,17 @@ class ControlOptionsCommon(ClientOptionsBase):
 
     def __init__(
         self,
-        count: Optional[int] = 1,
-        timeout: Optional[int] = None,
-        sync_mode: Optional[str] = None,
-        close_sleep: Optional[int] = None
+        count: 'Optional[int]' = 1,
+        timeout: 'Optional[int]' = None,
+        sync_mode: 'Optional[str]' = None,
+        close_sleep: 'Optional[int]' = None
     ) -> None:
         self.count: Optional[int] = count
         self.timeout: Optional[int] = timeout
         self.sync_mode: Optional[str] = sync_mode
         self.close_sleep: Optional[int] = close_sleep
 
-    def valid_options(self) -> List[Union[Toggle, Prefixed, KWOption, ListOption]]:
+    def valid_options(self) -> 'List[Union[Toggle, Prefixed, KWOption, ListOption]]':
         return [
             Prefixed('count', '--count'),
             Prefixed('timeout', '--timeout'),
@@ -91,12 +91,12 @@ class ControlOptionsSenderReceiver(ControlOptionsCommon):
     def __init__(
         self,
         count: int = 1,
-        timeout: Optional[int] = None,
-        sync_mode: Optional[str] = None,
-        close_sleep: Optional[int] = None,
-        duration: Optional[int] = None,
-        duration_mode: Optional[str] = None,
-        capacity: Optional[int] = None
+        timeout: 'Optional[int]' = None,
+        sync_mode: 'Optional[str]' = None,
+        close_sleep: 'Optional[int]' = None,
+        duration: 'Optional[int]' = None,
+        duration_mode: 'Optional[str]' = None,
+        capacity: 'Optional[int]' = None
     ) -> None:
         super(ControlOptionsSenderReceiver, self).__init__(
             count, timeout, sync_mode, close_sleep
@@ -105,7 +105,7 @@ class ControlOptionsSenderReceiver(ControlOptionsCommon):
         self.duration_mode: Optional[str] = duration_mode
         self.capacity: Optional[int] = capacity
 
-    def valid_options(self) -> List[Union[Toggle, Prefixed, KWOption, ListOption]]:
+    def valid_options(self) -> 'List[Union[Toggle, Prefixed, KWOption, ListOption]]':
         return super(ControlOptionsSenderReceiver, self).valid_options() + [
             Prefixed('duration', '--duration'),
             Prefixed('duration-mode', '--duration-mode'),
@@ -121,12 +121,12 @@ class ControlOptionsReceiver(ControlOptionsSenderReceiver):
     def __init__(
         self,
         count: int = 1,
-        timeout: Optional[int] = None,
-        sync_mode: Optional[str] = None,
-        close_sleep: Optional[int] = None,
-        duration: Optional[int] = None,
-        duration_mode: Optional[str] = None,
-        capacity: Optional[int] = None,
+        timeout: 'Optional[int]' = None,
+        sync_mode: 'Optional[str]' = None,
+        close_sleep: 'Optional[int]' = None,
+        duration: 'Optional[int]' = None,
+        duration_mode: 'Optional[str]' = None,
+        capacity: 'Optional[int]' = None,
         dynamic: bool = False
     ) -> None:
         super(ControlOptionsReceiver, self).__init__(
@@ -134,7 +134,7 @@ class ControlOptionsReceiver(ControlOptionsSenderReceiver):
         )
         self.dynamic: bool = dynamic
 
-    def valid_options(self) -> List[Union[Toggle, Prefixed, KWOption, ListOption]]:
+    def valid_options(self) -> 'List[Union[Toggle, Prefixed, KWOption, ListOption]]':
         return super(ControlOptionsReceiver, self).valid_options()
 
 
@@ -145,13 +145,13 @@ class LoggingOptionsCommon(ClientOptionsBase):
 
     def __init__(
         self,
-        log_lib: Optional[str] = None,
-        log_stats: Optional[str] = None
+        log_lib: 'Optional[str]' = None,
+        log_stats: 'Optional[str]' = None
     ) -> None:
         self.log_lib: Optional[str] = log_lib
         self.log_stats: Optional[str] = log_stats
 
-    def valid_options(self) -> List[Union[Toggle, Prefixed, KWOption, ListOption]]:
+    def valid_options(self) -> 'List[Union[Toggle, Prefixed, KWOption, ListOption]]':
         return [Prefixed('log-lib', '--log-lib'), Prefixed('log-stats', '--log-stats')]
 
 
@@ -162,14 +162,14 @@ class LoggingOptionsSenderReceiver(LoggingOptionsCommon):
 
     def __init__(
         self,
-        log_lib: Optional[str] = None,
-        log_stats: Optional[str] = None,
-        logs_msgs: Optional[str] = None
+        log_lib: 'Optional[str]' = None,
+        log_stats: 'Optional[str]' = None,
+        logs_msgs: 'Optional[str]' = None
     ) -> None:
         super(LoggingOptionsSenderReceiver, self).__init__(log_lib, log_stats)
         self.log_msgs: Optional[str] = logs_msgs
 
-    def valid_options(self) -> List[Union[Toggle, Prefixed, KWOption, ListOption]]:
+    def valid_options(self) -> 'List[Union[Toggle, Prefixed, KWOption, ListOption]]':
         return super(LoggingOptionsSenderReceiver, self).valid_options() + [
             Prefixed('log-msgs', '--log-msgs')
         ]
@@ -182,15 +182,15 @@ class TransactionOptionsSenderReceiver(ClientOptionsBase):
 
     def __init__(
         self,
-        tx_size: Optional[int] = None,
-        tx_action: Optional[str] = None,
-        tx_endloop_action: Optional[str] = None
+        tx_size: 'Optional[int]' = None,
+        tx_action: 'Optional[str]' = None,
+        tx_endloop_action: 'Optional[str]' = None
     ) -> None:
         self.tx_size: Optional[int] = tx_size
         self.tx_action: Optional[str] = tx_action
         self.tx_endloop_action: Optional[str] = tx_endloop_action
 
-    def valid_options(self) -> List[Union[Toggle, Prefixed, KWOption, ListOption]]:
+    def valid_options(self) -> 'List[Union[Toggle, Prefixed, KWOption, ListOption]]':
         return [
             Prefixed('tx-size', '--tx-size'),
             Prefixed('tx-action', '--tx-action'),
@@ -205,13 +205,13 @@ class ConnectionOptionsCommon(ClientOptionsBase):
 
     def __init__(
         self,
-        urls: Optional[str] = None,
-        reconnect: Optional[bool] = None,
-        reconnect_interval: Optional[int] = None,
-        reconnect_limit: Optional[int] = None,
-        reconnect_timeout: Optional[int] = None,
-        heartbeat: Optional[int] = None,
-        max_frame_size: Optional[int] = None
+        urls: 'Optional[str]' = None,
+        reconnect: 'Optional[bool]' = None,
+        reconnect_interval: 'Optional[int]' = None,
+        reconnect_limit: 'Optional[int]' = None,
+        reconnect_timeout: 'Optional[int]' = None,
+        heartbeat: 'Optional[int]' = None,
+        max_frame_size: 'Optional[int]' = None
     ) -> None:
         self.conn_urls: Optional[str] = urls
         self.conn_reconnect: Optional[bool] = reconnect
@@ -221,7 +221,7 @@ class ConnectionOptionsCommon(ClientOptionsBase):
         self.conn_heartbeat: Optional[int] = heartbeat
         self.conn_max_frame_size: Optional[int] = max_frame_size
 
-    def valid_options(self) -> List[Union[Toggle, Prefixed, KWOption, ListOption]]:
+    def valid_options(self) -> 'List[Union[Toggle, Prefixed, KWOption, ListOption]]':
         return [
             Prefixed('conn-urls', '--conn-urls'),
             Prefixed('conn-reconnect', '--conn-reconnect'),
@@ -238,10 +238,10 @@ class ConnectorOptions(ClientOptionsBase):
     Common options for connector client commands
     """
 
-    def __init__(self, obj_ctrl: Optional[str] = None) -> None:
+    def __init__(self, obj_ctrl: 'Optional[str]' = None) -> None:
         self.obj_ctrl: Optional[str] = obj_ctrl
 
-    def valid_options(self) -> List[Union[Toggle, Prefixed, KWOption, ListOption]]:
+    def valid_options(self) -> 'List[Union[Toggle, Prefixed, KWOption, ListOption]]':
         return [Prefixed('obj-ctrl', '--obj-ctrl')]
 
 
@@ -260,7 +260,7 @@ class LinkOptionsSenderReceiver(ClientOptionsBase):
         self.link_at_least_once: bool = link_at_least_once
         self.link_at_most_once: bool = link_at_most_once
 
-    def valid_options(self) -> List[Union[Toggle, Prefixed, KWOption, ListOption]]:
+    def valid_options(self) -> 'List[Union[Toggle, Prefixed, KWOption, ListOption]]':
         return [
             Toggle('link-durable', '--link-durable'),
             Toggle('link-at-least-once', '--link-at-least-once'),
@@ -278,14 +278,14 @@ class LinkOptionsReceiver(LinkOptionsSenderReceiver):
         link_durable: bool = False,
         link_at_least_once: bool = False,
         link_at_most_once: bool = False,
-        link_dynamic_node_properties: Optional[str] = None
+        link_dynamic_node_properties: 'Optional[str]' = None
     ) -> None:
         super(LinkOptionsReceiver, self).__init__(
             link_durable, link_at_least_once, link_at_most_once
         )
         self.link_dynamic_node_properties = link_dynamic_node_properties
 
-    def valid_options(self) -> List[Union[Toggle, Prefixed, KWOption, ListOption]]:
+    def valid_options(self) -> 'List[Union[Toggle, Prefixed, KWOption, ListOption]]':
         return super(LinkOptionsReceiver, self).valid_options() + [
             Prefixed('link-dynamic-node-properties', '--link-dynamic-node-properties')
         ]
@@ -298,24 +298,24 @@ class MessageOptionsSender(ClientOptionsBase):
 
     def __init__(
         self,
-        msg_id: Optional[str] = None,
-        msg_subject: Optional[str] = None,
-        msg_address: Optional[str] = None,
-        msg_reply_to: Optional[str] = None,
-        msg_durable: Optional[str] = None,
-        msg_ttl: Optional[int] = None,
-        msg_priority: Optional[str] = None,
-        msg_correlation_id: Optional[str] = None,
-        msg_user_id: Optional[str] = None,
-        msg_group_id: Optional[str] = None,
-        msg_group_seq: Optional[str] = None,
-        msg_property: Optional[str] = None,
-        msg_content_map_item: Optional[str] = None,
-        msg_content_list_item: Optional[str] = None,
-        msg_content_from_file: Optional[str] = None,
-        msg_content: Optional[ApplicationData] = None,
-        msg_content_type: Optional[str] = None,
-        content_type: Optional[str] = None
+        msg_id: 'Optional[str]' = None,
+        msg_subject: 'Optional[str]' = None,
+        msg_address: 'Optional[str]' = None,
+        msg_reply_to: 'Optional[str]' = None,
+        msg_durable: 'Optional[str]' = None,
+        msg_ttl: 'Optional[int]' = None,
+        msg_priority: 'Optional[str]' = None,
+        msg_correlation_id: 'Optional[str]' = None,
+        msg_user_id: 'Optional[str]' = None,
+        msg_group_id: 'Optional[str]' = None,
+        msg_group_seq: 'Optional[str]' = None,
+        msg_property: 'Optional[str]' = None,
+        msg_content_map_item: 'Optional[str]' = None,
+        msg_content_list_item: 'Optional[str]' = None,
+        msg_content_from_file: 'Optional[str]' = None,
+        msg_content: 'Optional[ApplicationData]' = None,
+        msg_content_type: 'Optional[str]' = None,
+        content_type: 'Optional[str]' = None
     ) -> None:
         self.msg_id: Optional[str] = msg_id
         self.msg_subject: Optional[str] = msg_subject
@@ -336,7 +336,7 @@ class MessageOptionsSender(ClientOptionsBase):
         self.msg_content_type: Optional[str] = msg_content_type
         self.content_type: Optional[str] = content_type
 
-    def valid_options(self) -> List[Union[Toggle, Prefixed, KWOption, ListOption]]:
+    def valid_options(self) -> 'List[Union[Toggle, Prefixed, KWOption, ListOption]]':
         return [
             Prefixed('msg-id', '--msg-id'),
             Prefixed('msg-subject', '--msg-subject'),
@@ -364,15 +364,15 @@ class ReceiverOptions(ClientOptionsBase):
 
     def __init__(
         self,
-        process_reply_to: Optional[str] = None,
-        action: Optional[str] = None,
-        recv_browse: Optional[bool] = None
+        process_reply_to: 'Optional[str]' = None,
+        action: 'Optional[str]' = None,
+        recv_browse: 'Optional[bool]' = None
     ) -> None:
         self.process_reply_to: Optional[str] = process_reply_to
         self.action: Optional[str] = action
         self.recv_browse: Optional[bool] = recv_browse
 
-    def valid_options(self) -> List[Union[Toggle, Prefixed, KWOption, ListOption]]:
+    def valid_options(self) -> 'List[Union[Toggle, Prefixed, KWOption, ListOption]]':
         return [
             Prefixed('process-reply-to', '--process-reply-to'),
             Prefixed('action', '--action'),
@@ -385,8 +385,8 @@ class ReactorOptionsSenderReceiver(ClientOptionsBase):
     Common reactor options for all Sender and Receiver client commands
     """
 
-    def __init__(self, reactor_auto_settle_off: Optional[bool] = None) -> None:
-        self.reactor_auto_settle_off: Optional[bool] = reactor_auto_settle_off
+    def __init__(self, reactor_auto_settle_off: 'Optional[bool]' = None) -> None:
+        self.reactor_auto_settle_off: 'Optional[bool]' = reactor_auto_settle_off
 
-    def valid_options(self) -> List[Union[Toggle, Prefixed, KWOption, ListOption]]:
+    def valid_options(self) -> 'List[Union[Toggle, Prefixed, KWOption, ListOption]]':
         return [Toggle('reactor-auto-settle-off', '--reactor-auto-settle-off')]
