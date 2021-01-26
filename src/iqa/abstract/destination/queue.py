dce0f53515f97e478456b3988bccd9a1f3148692
Roman Vais <rvais@redhat.com>
2021-01-26 12:26:43 +0100

    Surround typing annotations with single quotes abstract/destination/queue.py
    Typing annotatios of objects that are not imported for the runtime but
    serve only for static type checking must be surrounded in singlequotes
    if used in method signatures.

--- src/iqa/abstract/destination/queue.py
+++ src/iqa/abstract/destination/queue.py
@@ -4,13 +4,13 @@ Represents a generic Queue entity.
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from typing import Optional, List, Union
+    from typing import Optional
     from iqa.abstract.destination.routing_type import RoutingType
     from iqa.abstract.destination.address import Address
 
 
 class Queue:
-    def __init__(self, name: str, routing_type: RoutingType, address: Optional[Address] = None) -> None:
+    def __init__(self, name: str, routing_type: 'RoutingType', address: 'Optional[Address]' = None) -> None:
         self.name: str = name
         self.routing_type: RoutingType = routing_type
         self._address: Address = address
@@ -24,11 +24,11 @@ class Queue:
         return '%s::%s' % (self.address.name, self.name)
 
     @property
-    def address(self) -> Optional[Address]:
+    def address(self) -> 'Optional[Address]':
         return self._address
 
     @address.setter
-    def set_address(self, address: Address):
+    def set_address(self, address: 'Address') -> None:
         if self._address is not None:
             raise AttributeError("Queue %s already has assigned address %s. Address cannot be reassigned.")
         if address is None:
