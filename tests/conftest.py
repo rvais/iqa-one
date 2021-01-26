f5181cf012e12177addc2b2fea477e79a7def29f
Roman Vais <rvais@redhat.com>
2021-01-26 15:32:16 +0100

    Comment out most of the conftest file

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,23 +2,23 @@ import logging
 import os
 import pytest
 
-from iqa.system.executor.docker.executor_docker import ExecutorDocker
-from iqa.system.node import NodeDocker
-from iqa.system.node.node import Node
+# from iqa.system.executor.docker.executor_docker import ExecutorDocker
+# from iqa.system.node import NodeDocker
+# from iqa.system.node.node import Node
 
 logging.basicConfig(level=logging.DEBUG)
 
 
-@pytest.fixture
-def node(docker_services) -> Node:
-    executor: ExecutorDocker = ExecutorDocker(
-        container_name='sshd-container'
-    )
-    node: NodeDocker = NodeDocker(hostname="sshd-container", executor=executor)
-    return node
-
-
-@pytest.fixture(scope="session")
-def docker_compose_file(pytestconfig):
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    return os.path.join(str(dir_path), "docker-compose.yml")
+# @pytest.fixture
+# def node(docker_services) -> Node:
+#     executor: ExecutorDocker = ExecutorDocker(
+#         container_name='sshd-container'
+#     )
+#     node: NodeDocker = NodeDocker(hostname="sshd-container", executor=executor)
+#     return node
+#
+#
+# @pytest.fixture(scope="session")
+# def docker_compose_file(pytestconfig):
+#     dir_path = os.path.dirname(os.path.realpath(__file__))
+#     return os.path.join(str(dir_path), "docker-compose.yml")
