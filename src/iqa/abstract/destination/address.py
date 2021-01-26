93f349e0abfc9a71ccc8e44a412f80162d26db79
Roman Vais <rvais@redhat.com>
2021-01-26 12:22:08 +0100

    Surround typing annotations with single quotes abstract/destination/address.py
    Typing annotatios of objects that are not imported for the runtime but
    serve only for static type checking must be surrounded in singlequotes
    if used in method signatures.

--- src/iqa/abstract/destination/address.py
+++ src/iqa/abstract/destination/address.py
@@ -14,8 +14,8 @@ class Address:
     def __init__(
         self,
         name: str,
-        routing_type: RoutingType,
-        queues: Optional[Union[Queue, List[Queue]]] = None
+        routing_type: 'RoutingType',
+        queues: 'Optional[Union[Queue, List[Queue]]]' = None
     ) -> None:
         self.name: str = name
         self.routing_type: RoutingType = routing_type
@@ -31,10 +31,10 @@ class Address:
     def queues(self) -> list:
         return self._queues.copy()
 
-    def add_queue(self, queue: Queue) -> None:
+    def add_queue(self, queue: 'Queue') -> None:
         self._queues.append(queue)
 
-    def has_queue(self, queue: Union[Queue, str]) -> bool:
+    def has_queue(self, queue: 'Union[Queue, str]') -> bool:
         if isinstance(queue, str):
             for q in self._queues:
                 if q.name == queue:
