c88989740b150288ee16c178369c6db3d5ef55f8
Roman Vais <rvais@redhat.com>
2021-02-16 11:57:24 +0100

    Updating Typing annotations in method signatures of executor for localhost

--- src/iqa/system/executor/localhost/executor_local.py
+++ src/iqa/system/executor/localhost/executor_local.py
@@ -3,6 +3,8 @@ from iqa.system.executor.localhost.execution_local import ExecutionProcess
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
+    from os import PathLike
+    from typing import Optional, Dict, Union
     from iqa.system.command.command_base import CommandBase
     from iqa.system.executor.base.execution import ExecutionBase
 
@@ -17,9 +19,23 @@ class ExecutorLocal(ExecutorBase):
     Executes a given command locally.
     """
 
-    def __init__(self, name: str = 'ExecutorLocal', **kwargs) -> None:
+    def __init__(
+        self,
+        host: 'Optional[str]' = None,
+        port: 'Optional[int]' = None,
+        user: 'Optional[str]' = None,
+        password: 'Optional[str]' = None,
+        ssh_key_path: 'Optional[Union[str, bytes, PathLike]]' = None,
+        ssh_key_passphrase: 'Optional[Union[str, bytes, PathLike]]' = None,
+        known_hosts_path: 'Optional[Union[str, bytes, PathLike]]' = None,
+        **kwargs
+    ) -> None:
+        args: Dict = locals()
+        del args["self"]
+        del args["kwargs"]
+        del args["__class__"]
+        kwargs.update(args)
         super(ExecutorLocal, self).__init__(**kwargs)
-        self._name: str = name
 
     @staticmethod
     def implementation() -> str:
