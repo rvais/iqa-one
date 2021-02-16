5d75133ab86f1ce7a0c3e29c979c2fd70f23543b
Roman Vais <rvais@redhat.com>
2021-02-16 11:54:44 +0100

    Updating Typing annotations in method signatures of executor asynclocalhost

--- src/iqa/system/executor/asynclocalhost/executor.py
+++ src/iqa/system/executor/asynclocalhost/executor.py
@@ -5,7 +5,8 @@ from iqa.system.executor.base.executor import ExecutorBase
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from typing import Optional
+    from os import PathLike
+    from typing import Optional, Dict, Union
     from iqa.system.command.command_base import CommandBase
 
 
@@ -13,19 +14,27 @@ class ExecutorAsyncio(ExecutorBase):
     """ Executor implementation for localhost AsyncIO executions
     """
 
-    def __init__(self, user: str = 'root', password: 'Optional[str]' = None, **kwargs) -> None:
-
-        super(ExecutorAsyncio).__init__(**kwargs)
-        self._user = user
-        self._password = password
+    def __init__(
+        self,
+        host: 'Optional[str]' = None,
+        port: 'Optional[int]' = None,
+        user: 'Optional[str]' = None,
+        password: 'Optional[str]' = None,
+        ssh_key_path: 'Optional[Union[str, bytes, PathLike]]' = None,
+        ssh_key_passphrase: 'Optional[Union[str, bytes, PathLike]]' = None,
+        known_hosts_path: 'Optional[Union[str, bytes, PathLike]]' = None,
+        **kwargs
+    ) -> None:
+        args: Dict = locals()
+        del args["self"]
+        del args["kwargs"]
+        del args["__class__"]
+        kwargs.update(args)
+        super(ExecutorAsyncio, self).__init__(**kwargs)
 
     @staticmethod
     def implementation() -> str:
         return "async local"
-
-    @property
-    def name(self) -> str:
-        return 'Executor class for asynchronous localhost execution'
 
     async def _execute(self, command: 'CommandBase') -> ExecutionAsyncio:
         execution = ExecutionAsyncio(command)
