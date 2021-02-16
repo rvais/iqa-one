c62629c703af002e91c8d55a31966362b3ae3811
Roman Vais <rvais@redhat.com>
2021-02-16 11:57:46 +0100

    Updating Typing annotations in method signatures of ssh executor

--- src/iqa/system/executor/ssh/executor_ssh_old.py
+++ src/iqa/system/executor/ssh/executor_ssh_old.py
@@ -5,7 +5,8 @@ from iqa.system.executor.localhost.execution_local import ExecutionProcess
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from typing import Optional
+    from os import PathLike
+    from typing import Optional, Dict, Union
     from iqa.system.command.command_base import CommandBase
 
 """
@@ -21,39 +22,44 @@ class ExecutorSshOld(ExecutorBase):
     otherwise it may run indefinitely or timeout.
     """
 
-    implementation = 'ssh'
-    name: str = 'Executor class for SSH remote execution'
-
     def __init__(
         self,
-        hostname: str,
-        port: str = '22',
-        user: str = 'root',
-        ssl_private_key: 'Optional[str]' = None,
-        name: str = 'ExecutorSsh',
+        host: 'Optional[str]' = None,
+        port: 'Optional[int]' = None,
+        user: 'Optional[str]' = None,
+        password: 'Optional[str]' = None,
+        ssh_key_path: 'Optional[Union[str, bytes, PathLike]]' = None,
+        ssh_key_passphrase: 'Optional[Union[str, bytes, PathLike]]' = None,
+        known_hosts_path: 'Optional[Union[str, bytes, PathLike]]' = None,
         **kwargs
     ) -> None:
-        super(ExecutorSshOld, self).__init__()
-        self.hostname: str = kwargs.get('executor_hostname', hostname)
-        self.port: str = kwargs.get('executor_port', port)
-        self.user: str = kwargs.get('executor_user', user)
-        self.name: str = kwargs.get('executor_name', name)
-        self.ssl_private_key: Optional[str] = kwargs.get(
-            'executor_ssl_private_key', ssl_private_key
-        )
+        args: Dict = locals()
+        del args["self"]
+        del args["kwargs"]
+        del args["__class__"]
+        kwargs.update(args)
+        super(ExecutorSshOld, self).__init__(**kwargs)
+
+        missing = self._check_required_args(['host'], **kwargs)
+        if missing:
+            raise ValueError(f"One or more mandatory arguments are missing: [{ ', '.join(missing)}]")
+
+    @staticmethod
+    def implementation() -> str:
+        return "ssh"
 
     def _execute(self, command: 'CommandBase') -> ExecutionProcess:
-        ssh_args: list = ['ssh', '-p', '%s' % self.port]
+        ssh_args: list = ['ssh', '-p', '%s' % self._port]
 
         # If an SSL private key given, use it
-        if self.ssl_private_key is not None and os.path.isfile(self.ssl_private_key):
-            self._logger.debug('Using SSL Private Key - %s' % self.ssl_private_key)
-            ssh_args += ['-i', self.ssl_private_key]
+        if self._ssh_key_path is not None and os.path.isfile(self._ssh_key_path):
+            self._logger.debug('Using SSL Private Key - %s' % self._ssh_key_path)
+            ssh_args += ['-i', self._ssh_key_path]
 
         # if not self.stricthostkeychecking:
         #     self._logger.debug('Using StrictHostKeyChecking no')
         #     ssh_args += ['-o', '"StrictHostKeyChecking no"']
 
-        ssh_args += ['%s@%s' % (self.user, self.hostname)]
+        ssh_args += ['%s@%s' % (self._user, self._host)]
 
         return ExecutionProcess(command, executor=self, modified_args=ssh_args + command.args)
