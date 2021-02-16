87b0f384ce9938dd538f7dfc471734571124d09c
Roman Vais <rvais@redhat.com>
2021-02-16 12:20:24 +0100

    Fix walk package utility

--- src/iqa/utils/walk_package.py
+++ src/iqa/utils/walk_package.py
@@ -4,14 +4,16 @@ from importlib import resources
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from typing import Optional, Union, Type, List, Any, Iterator
+    from typing import Optional, Union, Type, List, Any, Set
     from os import PathLike
 
 
 def walk_package_and_import(
         package_path: 'Union[PathLike[Any], str]',
         superclass: 'Optional[Type]' = None,
-        skip_modules: 'Optional[List[PathLike[Any]]]' = None
+        skip_modules: 'Optional[List[PathLike[Any]]]' = None,
+        current_depth: int = 0,
+        max_depth: int = 2
 ) -> 'List[Type]':
     """Recursively walk the supplied package to retrieve all plugins
        :param package_path: path to package that is supposed to be iterated
@@ -19,38 +21,45 @@ def walk_package_and_import(
        of the file that calls this function.
        :param superclass: 'Type' object that represents
        :param skip_modules: list of paths of packages to be skipped and not imported again
+       :param current_depth: current package depth
+       :param max_depth: how deep to go to discover new subpackages
        :return imported types
     """
 
-    results: List[type] = []
+    results: Set[type] = set()
+
+    if current_depth > max_depth:
+        return []
 
     if skip_modules is None:
         skip_modules = []
 
     if isinstance(package_path, list):
         for pp in package_path:
-            results.extend(walk_package_and_import(pp, superclass, skip_modules))
-        return results
+            results.update(walk_package_and_import(pp, superclass, skip_modules))
+        return list(results)
 
     files: List[str] = list(resources.contents(package_path))
 
     modules: List[str] = [f[:-3] for f in files if f.endswith(".py") and f[0] != "_"]
-    submodules: List[str] = [f for f in files if not f.endswith(".py")]
-
-    # Filter out submodules to skip
-    for skip in skip_modules:
-        submodules = [subm for subm in submodules if not subm.startswith(skip)]
+    subpackages: List[str] = [f for f in files if not f.endswith(".py")]
 
     # import modules from subpackages/submodules
-    for subm in submodules:
-        module_path: str = f"{package_path}.{subm}"
-        results.extend(walk_package_and_import(module_path, superclass, skip_modules))
+    for subpkg in subpackages:
+        subpackage_path: str = f"{package_path}.{subpkg}"
+        if subpackage_path in skip_modules:
+            continue
+        results.update(walk_package_and_import(subpackage_path, superclass, skip_modules, current_depth + 1, max_depth))
+        skip_modules.append(subpackage_path)
 
     for mod in modules:
         module_path: str = f"{package_path}.{mod}"
+        if module_path in skip_modules:
+            continue
+
         imported = importlib.import_module(module_path)
         classes = inspect.getmembers(imported, inspect.isclass)
         if superclass is not None:
-            results.extend([cls for _, cls in classes if isinstance(cls, superclass)])
+            results.update([cls for _, cls in classes if issubclass(cls, superclass) and not inspect.isabstract(cls)])
 
-    return results
+    return list(results)
