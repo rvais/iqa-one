5d90d08cd188bb9984cb6e5da0e9818993b4d3f4
Roman Vais <rvais@redhat.com>
2021-02-17 18:09:33 +0100

    Create new 'ClientsFactory' class implementation

--- src/iqa/components/implementations/clients/clients_factory.py
+++ src/iqa/components/implementations/clients/clients_factory.py
@@ -1,17 +1,79 @@
 import logging
 
-from iqa.components.clients.external.client_external import ClientExternal
-from iqa.system.executor.executor import ExecutorBase
-from iqa.system.node.node import Node
+from iqa.components.implementations.clients.external.client_external import ClientExternal
+from iqa.utils.walk_package import walk_package_and_import
+from iqa.components.implementations.component_factory import SpecificComponentFactory
+from iqa.abstract.client.client import Client
+from iqa.abstract.client.sender import Sender
+from iqa.abstract.client.receiver import Receiver
+from iqa.abstract.client.messaging_client import MessagingClient
+from iqa.components.implementations.clients import __package__
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import List, Type, Dict
+    from iqa.components.abstract.component import Component
+    from iqa.system.node.base.node import Node
 
 
-class ClientFactory(object):
+class ClientFactory(SpecificComponentFactory):
+    __client_implementations: 'List[Type[Client]]' = walk_package_and_import(__package__, Client)
+    __type: 'Type[Component]' = Client
+    __client_type_mapping: 'Dict[str, Type[MessagingClient]]' = {
+        'sender': Sender,
+        'receiver': Receiver,
+        'connector': MessagingClient
+    }
+
+    @classmethod
+    def get_type(cls) -> 'Type[Component]':
+        return cls.__type
+
+    @classmethod
+    def get_available_implementations(cls) -> 'List[Type[Client]]':
+        return cls.__client_implementations.copy()
+
+    @staticmethod
+    def create_component(implementation: str, node: 'Node', **kwargs) -> 'Client':
+        # determine specific client type first
+        client_type: Type[Client] = MessagingClient
+        if 'type' in kwargs.keys() and kwargs['type'] in ClientFactory.__client_type_mapping.keys():
+            client_type = ClientFactory.__client_type_mapping[kwargs['type']]
+        elif 'type' in kwargs.keys():
+            for cls in ClientFactory.get_available_implementations():
+                if cls.__name__ == kwargs['type']:
+                    client_type = cls
+                    break
+
+        for client in ClientFactory.get_available_implementations():
+            # Ignore clients with different implementation
+            if client.implementation != implementation or not issubclass(client, client_type):
+                continue
+
+            # Make sure component has a name
+            name: str
+            if 'name' not in kwargs.keys():
+                name = f"{kwargs['type']}-{client.__name__}-{node.hostname}"
+            else:
+                name = kwargs['name']
+                del kwargs['name']
+
+            return client(name=name, node=node, **kwargs)
+
+        err: str = f"Invalid client implementation '{implementation}' and/or client type '{client_type.__name__}'."
+        raise ValueError(err)
+
+
+class OldClientFactory(object):
     # Static element to store all available implementations
     _implementations: list = []
 
     @staticmethod
     def create_clients(
-        implementation: str, node: Node, executor: ExecutorBase, **kwargs
+        implementation: str, node: Node,
+        executor,  #: ExecutorBase,
+        **kwargs
     ) -> list:
         for cl in ClientExternal.__subclasses__():
 
